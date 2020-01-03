507641e457fea6058c2f4436f73ebe7822a6bb8f
Tim Wagner <t.wagner@techdivision.com>
2020-01-03 14:06:58 +0100

    Update PHP unit tests

--- tests/unit/Subjects/BunchSubjectTest.php
+++ tests/unit/Subjects/BunchSubjectTest.php
@@ -20,6 +20,7 @@
 
 namespace TechDivision\Import\Category\Subjects;
 
+use PHPUnit\Framework\TestCase;
 use Doctrine\Common\Collections\ArrayCollection;
 
 /**
@@ -31,7 +32,7 @@ use Doctrine\Common\Collections\ArrayCollection;
  * @link      https://github.com/techdivision/import-category
  * @link      http://www.techdivision.com
  */
-class BunchSubjectTest extends \PHPUnit_Framework_TestCase
+class BunchSubjectTest extends TestCase
 {
 
     /**
@@ -46,7 +47,7 @@ class BunchSubjectTest extends \PHPUnit_Framework_TestCase
      * This method is called before a test is executed.
      *
      * @return void
-     * @see \PHPUnit_Framework_TestCase::setUp()
+     * @see \PHPUnit\Framework\TestCase::setUp()
      */
     protected function setUp()
     {
